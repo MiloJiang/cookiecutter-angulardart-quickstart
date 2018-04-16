3354c02b37e8c769a6989a91257ff139d436c6d5
jianglei <jianglei@360.cn>
2018-04-16 16:31:00 +0800

    追加文件

--- {{cookiecutter.project_dir}}/lib/src/dashboard_component.dart
+++ {{cookiecutter.project_dir}}/lib/src/dashboard_component.dart
@@ -3,13 +3,16 @@ import 'dart:async';
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 
+//Add dashboard
+import 'package:angular_components/material_button/material_button.dart';
+
 import 'hero.dart';
 import 'hero_service.dart';
 
 @Component(
   selector: 'my-dashboard',
   templateUrl: 'dashboard_component.html',
-  directives: const [CORE_DIRECTIVES, ROUTER_DIRECTIVES],
+  directives: const [CORE_DIRECTIVES, ROUTER_DIRECTIVES, MaterialButtonComponent],
   styleUrls: const ['dashboard_component.css'],
 )
 
@@ -22,5 +25,11 @@ class DashboardComponent implements OnInit {
 
   Future<Null> ngOnInit() async {
     heroes = (await _heroService.getHeroes()).skip(1).take(4).toList();
+  }
+
+  var content = 'Click Me';
+  
+  void click() {
+    content = (content == 'Click Me' ? 'Good' : "Click Me");   
   }
 }
